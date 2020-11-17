02a63df8d82e1dac032a85530f93f34cbffc847c
Samir Nabil <sameer.arcons@yahoo.co.uk>
2020-11-17 15:58:52 +0200

    Add Product Model

---
+++ src/models/Product.ts
@@ -0,0 +1,19 @@
+export interface Product {
+  item_name: string;
+  gender_id: number;
+  size: string;
+  departName: string;
+  intlDesc: string;
+  material?: string;
+  materialAr?: string;
+  description?: string;
+  descriptionAr?: string;
+  long_desc: string;
+  dept_id: string;
+  msrp: string;
+  rtp: string;
+  color1_id: string;
+  colorDesc: string;
+  season_id: string;
+  systemPrice: string;
+}
